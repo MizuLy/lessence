522ca764ddec90860ab78f4c21fcae0f1c7442e1
MizuLy <optionalsa@gmail.com>
2025-10-21 17:13:46 +0700

    footer related

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ export default function Footer() {
         <h1 className="text-3xl font-josefinsans mb-3">About</h1>
         <p className="cursor-pointer">Terms & Conditions</p>
         <p className="cursor-pointer mt-2">Privacy Policy</p>
-        <p className="absolute bottom-5 t-2">
+        <p className="absolute bottom-5 mt-2">
           &copy; {new Date().getFullYear()} L’Essence. All rights reserved.
         </p>
       </div>
